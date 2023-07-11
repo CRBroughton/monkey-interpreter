a6fb84937c6501a90d6f12c0163978dde104ce66
CRBroughton <crbroughton@posteo.uk>
2023-07-11 18:58:22 +0100

    feat: :sparkles: add Identifier and LetStatement node structs

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/crbroughton/monkey-interpreter/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -8,6 +10,23 @@ type Statement interface {
 	Node
 	statementNode()
 }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 type Expression interface {
 	Node
