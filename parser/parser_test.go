58bafea27b331cb740ea7892a5473d2b74c6098e
CRBroughton <crbroughton@posteo.uk>
2023-07-11 19:35:19 +0100

    feat: :sparkles: add error logging to parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -19,6 +19,7 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkerParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -64,4 +65,20 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkerParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser had %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
